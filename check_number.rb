fba16f80d0ecc72a322b72d1a804d5bfe1c3da44
Robert Cook <fixyourface79@gmail.com>
2016-10-22 00:27:14 +0000

    array_exercise

---
+++ check_number.rb
@@ -0,0 +1,8 @@
+# check_number.rb
+
+arr = [1, 3, 5, 7, 9, 11]
+number = 3
+
+if arr.include?(number)
+    puts "#{number} is indeed in the array."
+end    
---
+++ exercise_7.rb
@@ -0,0 +1,11 @@
+# exercise_7.rb
+
+arr = [10, 20, 30, 40, 50, 60]
+arr2 = [] 
+
+arr.each do |n|
+    arr2 << n + 2
+end    
+    
+p "#{arr}"
+p "#{arr2}"
